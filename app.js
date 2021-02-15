caf906c671220b65bc5e87818a3a46ae627f362b
Stevenson Nelson <sonson521@gmail.com>
2021-02-15 13:51:37 -0500

    add prompt to code.

---
+++ app.js
@@ -0,0 +1,8 @@
+// prompt user to type name
+// get the value of the name
+// if its not noon yet write good morning
+// and if its pass noon write good afternoon
+// and if its pass 5 write good evening
+let answer = prompt('what is your name?');
+
+console.log(answer);
