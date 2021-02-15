3c806cc92b95858f4d2abd0b7290c326285a1fc5
Stevenson Nelson <sonson521@gmail.com>
2021-02-15 15:04:22 -0500

    add if statement.

--- app.js
+++ app.js
@@ -3,6 +3,19 @@
 // if its not noon yet write good morning
 // and if its pass noon write good afternoon
 // and if its pass 5 write good evening
-let answer = prompt('what is your name?');
+let usersName = prompt('what is your name?');
 
-console.log(answer);
+let greeting = document.querySelector('.greeting');
+
+let date = new Date();
+if (date.getHours() > 5 && date.getHours() < 11) {
+  greeting.innerHTML = `Good Morning ${usersName}`;
+}
+if (date.getHours() > 12 && date.getHours() < 16) {
+  greeting.innerHTML = `Good Afternoon ${usersName}`;
+}
+
+if (date.getHours() > 17 && date.getHours() < 19) {
+  greeting.innerHTML = `Good Evening ${usersName}`;
+}
+console.log(date);
